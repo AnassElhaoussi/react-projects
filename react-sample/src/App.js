6e8f3f91b14a4b046d9d493b1f20921b944693b8
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-09 23:17:39 +0200

    feat : working on react-sample by adding components and dealing with them

---
+++ react-sample/src/App.js
@@ -0,0 +1,18 @@
+
+import React from 'react'; 
+import Tweets from './Tweets'
+
+
+function myApp(){
+  return (
+
+    <div className="app">
+      
+      <h1 className='title'>Hello there are some tweets :</h1>
+      <Tweets/>
+
+    </div>
+  )
+}
+
+export default myApp;
---
+++ react-sample/src/Tweets.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function Tweets(){
+
+    
+    return(
+        <div className='tweet'>
+            <input type="text" placeholder='tweet someting' name="" id="" />
+            <h1>User Tweet :</h1>
+            <p>This is the user tweet</p>
+        </div>
+    )
+
+}
+
+
+export default Tweets;
