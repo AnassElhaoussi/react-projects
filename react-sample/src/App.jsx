4a592fa164d7a8d34eee5b77a45073741b42b187
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-11 16:12:42 +0200

    chore : adding react-movies-app folder

--- react-sample/src/App.jsx
+++ react-sample/src/App.jsx
@@ -4,23 +4,10 @@ import Tweets from './Tweets'
 
 
 function App(){
-  
-const [users, setUsers] = useState([
-      {name: "Anass", message:"Hello there"},
-      {name: "John", message:"Helloo"},
-      {name: "Petter", message:"Whatsup"}
-])
-
-
 
   return (
-    <div className="app">
-    <button>Click to show tweets</button>
-    {users.map(user => (
-      <Tweets name={user.name} message={user.message}/>
-    ))}
-
-    </div>
+    
+      
   )
 }
 
--- react-sample/src/Tweets.js
+++ react-sample/src/Tweets.js
@@ -5,7 +5,6 @@ function Tweets({name, message}){
 
     return(
         <div className='tweet'>
-            <input type="text" placeholder='tweet someting' name="" id="" />
             <h1>{name}</h1>
             <p>{message}</p>
         </div>
