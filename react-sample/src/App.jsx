4a2626d40490c1b7821b63d5fa6483b3edb0f917
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-11 18:36:54 +0200

    chore : adding some changes to the react-sample folder

--- react-sample/src/App.jsx
+++ react-sample/src/App.jsx
@@ -4,10 +4,22 @@ import Tweets from './Tweets'
 
 
 function App(){
+  
+  const [userInfos, setUserInfos] = useState([
+    {name: "Anass", message: "Hello there"},
+    {name: "Josh", message: "Please can you help me"},
+    {name: "John", message: "Wassup"}
+
+  ])
+
+
 
   return (
-    
-      
+    <div className='app'>
+      {userInfos.map(userInfo => (
+        <Tweets name={userInfo.name} message={userInfo.message}/>
+      ))}
+    </div>
   )
 }
 
