bfdde91513efac3b0e17fb8f73d28e213aa5787d
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-16 21:50:07 +0200

    adding main files and components to the src folder

---
+++ react-router/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <h1>Navigation</h1>
+            <ul>
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ react-router/src/components/Shop.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <h1>Shop page</h1>
+    )
+}
+
+export default Shop
