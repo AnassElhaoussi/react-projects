917c289c3fc9bedd4ac6fea273c11b06eaeb0acb
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-17 13:27:50 +0200

    adding links and switching between routes

--- react-sample/src/Nav.jsx
+++ react-sample/src/Nav.jsx
@@ -1,14 +1,21 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom';
  
 const Nav = () => {
     return(
         <nav>
             <h1>Navigation bar</h1>
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Tweets</li>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+                <li>
+                    <Link to='/tweets'>Tweets</Link>
+                </li>
             </ul>
         </nav>
     )
