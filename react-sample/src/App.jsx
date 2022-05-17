9caa182ac69ecec40edf0b4df1636d320ec91ff1
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-05-17 13:14:03 +0200

    adding each route to the routes

--- react-sample/src/App.jsx
+++ react-sample/src/App.jsx
@@ -1,23 +1,33 @@
 
 import React, { useState } from 'react'; 
-import {BrowserRouter, Route,Routes, Switch} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Nav from './Nav'
+import About from './About'
 import Tweets from './Tweets'
 
 function App(){
   
-
-  
   return (
     <div className='App'>
-      <BrowserRouter>
-        <Routes>
+      <Router>
+        <Nav />   
+        <Routes> 
+          <Route path='/about' element={<About />}/>
           <Route path="/tweets" element={<Tweets/>} />
-        </Routes>
-      </BrowserRouter>
+          <Route path='/' element={<HomePage />} />
+        </Routes>  
+      </Router>
     </div>
   )
 
   
+}
+
+const HomePage = () => {
+  return (
+    <h1>Hello it's the home page</h1>
+  )
+
 }
 
 
---
+++ react-sample/src/Nav.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+ 
+const Nav = () => {
+    return(
+        <nav>
+            <h1>Navigation bar</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Tweets</li>
+            </ul>
+        </nav>
+    )
+    
+}
+
+export default Nav;
