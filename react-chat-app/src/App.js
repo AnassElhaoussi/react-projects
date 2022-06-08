4fb3a4a582aa6700d8d5575479a4f2f9833cf39c
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-06-08 19:15:50 +0200

    chat app setup

---
+++ react-chat-app/src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className=' text-pink-600'>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
